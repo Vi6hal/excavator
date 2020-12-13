b1c60c0777ecb118a6c83f4715829665c8c9731c
noah <you@example.com>
2020-12-13 23:15:50 +0530

    added static folder

---
+++ assets/js/core.f760ab193096.js
@@ -0,0 +1,69 @@
+document.addEventListener('DOMContentLoaded', function () {
+  'use strict';
+  var the_loader=document.getElementById("675iu")
+  var tracker_id=the_loader.getAttribute('data-key')
+  var redirect_url=the_loader.getAttribute('data-redirect')
+  var BASE_URL = window.location.origin
+  var requestUrl = "http://ip-api.com/json";
+  var xhr_ip = new XMLHttpRequest()
+  xhr_ip.open('GET', requestUrl, true)
+  xhr_ip.send();
+  xhr_ip.onload = () =>{
+    if (xhr_ip.status >= 200 && xhr_ip.status < 300)
+    {
+      fallback_default(JSON.parse(xhr_ip.responseText));
+    }
+    else{
+        fallback_default();
+      }
+  }
+  function fallback_default(additional_data={})
+  { 
+    // if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+
+    // }
+    var xhr = new XMLHttpRequest()
+    var timezone_offset = new Date().toString();
+    var packet={
+      "tracker":tracker_id,
+      "tzof": timezone_offset,
+      "gpu": getVideoCardInfo().renderer,
+      "OS": navigator.oscpu,
+      "platform": navigator.platform,
+      "language":navigator.language,
+      'ua':navigator.userAgent,
+      'screen_size':window.screen.width+'X'+window.screen.height
+    };
+    var final_dict=Object.assign({},packet,additional_data)
+    
+    xhr.open('POST', BASE_URL+'/welcome', true)
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify(final_dict));
+    xhr.onload = () => {
+      if (xhr.status >= 200 && xhr.status < 300) 
+      {
+        console.log(xhr.responseText);
+        window.location.replace(redirect_url);
+      }
+      else
+      {
+        window.location.replace(redirect_url);
+      }
+    };
+  }
+  function getVideoCardInfo() {
+    const gl = document.createElement('canvas').getContext('webgl');
+    if (!gl) {
+      return {
+        error: "NA",
+      };
+    }
+    const debugInfo = gl.getExtension('WEBGL_debug_renderer_info');
+    return debugInfo ? {
+      vendor: gl.getParameter(debugInfo.UNMASKED_VENDOR_WEBGL),
+      renderer: gl.getParameter(debugInfo.UNMASKED_RENDERER_WEBGL),
+    } : {
+      error: "NA",
+    };
+  }
+});
---
+++ assets/js/home.00634f54416c.js
@@ -0,0 +1,160 @@
+$( document ).ready(function() 
+{
+	M.AutoInit();
+	var BASE_URL = $(location).attr("origin");
+	var csrfmiddlewaretoken=$("input[name=csrfmiddlewaretoken]").val()
+	function async_updateinfo()
+	{
+		var managing_code=$('#track_code').text();
+		if(managing_code == undefined |managing_code == ''){}
+		else{
+			fetchRecords(managing_code)
+		}
+	}
+	$('#refresh_logs').on("click",function(e)
+	{
+		async_updateinfo();
+	});
+   function formPOSTHandler(formData)
+   {
+		var search_field= $('#searchIDorURL')
+		if(search_field.val().length == 6)
+		{
+			DoAjax(
+				{'access_key':search_field.val(),'csrfmiddlewaretoken':csrfmiddlewaretoken},
+				BASE_URL+'/load',
+				'POST',
+				onLoadExisting,"Checking existing Logger")
+
+		}
+		else
+		{
+			DoAjax({'access_key':search_field.val(),'csrfmiddlewaretoken':csrfmiddlewaretoken},BASE_URL+'/create','POST',onNewCreate,"Creating Logger")
+		}
+		ResetFields(formData);	
+   }
+   function onLoadExisting(response={})
+   {  	
+	   if(response.origin_url != false)
+		   	{
+				appendreqData("Orignal Url",response.origin_url,'origin_url')
+				appendreqData("New Url ",BASE_URL+"/"+response.tracking_code,'managing_code')
+				appendreqData("Tracking Code ",response.managing_code,'track_code')
+				fetchRecords(response.managing_code);
+				showresults();
+			}
+		else
+		{
+			M.toast({html: "Sorry not found",displayLength:3500});
+		}
+		
+	   }
+	function fetchRecords(mcode)
+	{
+		DoAjax(
+			{'access_key':mcode,'csrfmiddlewaretoken':csrfmiddlewaretoken},
+			BASE_URL+'/load_results',
+			'POST',
+			updateRecords,"Loading results")
+	}
+	function updateRecords(response={})
+	{
+		$('#result_records tr').remove();
+		$.each(response.data, function(key,value) {
+			appendRecordData(value)
+		});
+	}
+   function onNewCreate(response={}) 
+   {
+		appendreqData("Orignal Url",response.origin_url,'origin_url')
+		appendreqData("New Url ",BASE_URL+"/"+response.tracking_code,'managing_code')
+		appendreqData("Tracking Code ",response.managing_code,'track_code')
+		showresults();
+
+   }
+   function appendreqData(name,value,rec_id)
+   {
+   		$('.tracker_info').append("<tr><td>"+name+"</td><td id="+rec_id+">"+value+"</td></tr>");
+   }
+   function appendRecordData(recordSingle)
+   {
+	   var defau ="NA";
+		$('#result_records').append("<tr><td>"+recordSingle.timezone+
+		"</td><td>"+(recordSingle.request_api_ip == undefined || recordSingle.request_api_ip ==null ? defau :recordSingle.request_api_ip )+
+		"</td><td>"+(recordSingle.origin_country == undefined || recordSingle.origin_country ==null ? defau :recordSingle.origin_country )+
+		"</td><td>"+(recordSingle.user_ua == undefined || recordSingle.user_ua ==null ? defau :recordSingle.user_ua )+
+		"</td><td>"+(recordSingle.user_language == undefined || recordSingle.user_language == null ? defau:recordSingle.user_language)	+
+		"</td><td>"+(recordSingle.user_screensize == undefined || recordSingle.user_screensize == null ? defau : recordSingle.user_screensize)+
+		"</td><td>"+(recordSingle.user_isp == undefined || recordSingle.user_isp == null ? defau : recordSingle.user_isp)+
+		"</td><td>"+(recordSingle.device_gpu == undefined || recordSingle.device_gpu == null ? defau : recordSingle.device_gpu)+
+		"</td></tr>");
+   }
+   function ResetFields(form)
+   {
+   	form.reset();return;
+   }
+   function DoAjax(data,url,method,callback=false,message='loading')
+   {
+		$.ajax({
+			type:method,
+			url:url,
+			data:data,
+            cache: false,
+            beforeSend: function(xhr)
+            {
+				M.toast({html: message,displayLength:1500});
+            },
+            success: function (response) 
+            { 
+            		if($.isFunction(callback))
+            		{
+	            		callback(response);
+            		}
+            },
+            error: function (e)
+            {
+            	console.log(e);
+            	if($.isFunction(callback))
+            		{
+            			callback(e);
+            		}
+            }
+		});
+   }
+
+   function appenddummydata(search_field_dummy="NA")
+   {
+   			appendreqData('Orignal Url',search_field_dummy);
+			appendreqData('New URL ','https://grabify.link/V4PMN1');
+			appendreqData('Tracking Code ','JFXZOL');
+			appendreqData('Access Link','https://grabify.link/track/JFXZOL');
+   }
+
+   function hideresults()
+   {
+   		if(!$('.result_date').hasClass("hide"))
+			{
+				$('.result_date').addClass("hide");
+			}
+
+   }
+   function showresults()
+   {
+   		if($('.result_date').hasClass("hide"))
+			{
+				$('.result_date').removeClass("hide");
+			}
+   }
+   $('.form-globalsearch').submit(function(e)
+   {
+	if(this.method == "post")
+	{
+		hideresults();
+		$('.tracker_info tr').remove();
+		$('#result_records tr').remove();
+		e.preventDefault();
+		formPOSTHandler(this); return;
+	}
+});
+	
+ });
