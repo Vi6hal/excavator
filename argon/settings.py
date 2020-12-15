9acc996d24ce4a9ac3822d6482e51fb1a66554ef
noah <you@example.com>
2020-12-15 12:01:58 +0530

    fixing database issues

--- argon/settings.py
+++ argon/settings.py
@@ -76,9 +76,17 @@ WSGI_APPLICATION = 'argon.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+        'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'exkavator',
+        'USER': 'vishal',
+        'PASSWORD': 'vishal',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 db_from_env = dj_database_url.config(conn_max_age=500)
