4f8852558a81c1a129d0e669d58268607ff983e3
noah <you@example.com>
2020-12-15 20:16:02 +0530

    disabled debug mode

--- argon/settings.py
+++ argon/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 't8^4l#i#vzatc$%d)98d8*fh2(8vts*ydfgvja7zxczd+rqm^!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
