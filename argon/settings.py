6f3471d7b69ccd3b3231b597431f3c8f79d8b0a4
noah <you@example.com>
2020-12-18 00:36:50 +0530

    disabled -test

--- argon/settings.py
+++ argon/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'excavator'
 ]
 MIDDLEWARE = [
-    'django.middleware.gzip.GZipMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
